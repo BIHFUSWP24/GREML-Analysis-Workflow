f909aa0ea17258424ab6e5e26e4b70160d4ad2cd
Leonard <leonard.sima@fu-berlin.de>
2024-04-26 00:29:12 +0200

    clean workspace

--- workflow/scripts/filter_chromosome.py
+++
@@ -1,9 +0,0 @@
-import anndata
-
-data_path = '/sc-projects/sc-proj-dh-ag-eils-ml/genotype_data/100k_snp_all_chr.h5ad'
-output_path = '/sc-projects/sc-proj-dh-ukb-intergenics/analysis/development/lesi11/build/100k_snp_chr1.h5ad'
-output_path_tsv = '/sc-projects/sc-proj-dh-ukb-intergenics/analysis/development/lesi11/build/100k_snp_chr1.tsv'
-
-adata = anndata.read_h5ad(data_path, backed='r')
-adata_subset = adata[:, adata.var['GeneticChromosome'] == 1]
-adata_subset.write_h5ad(output_path)
--- workflow/scripts/test.R
+++
@@ -1,5 +0,0 @@
-source("/home/lesi11/testData/grm_io.r")
-
-# Read the GRM binary file
-grm <- ReadGRMBin("/home/lesi11/testData/test")
-write.csv(grm$fullGRM[0:50,0:50], file = "/home/lesi11/testData/test.csv")
